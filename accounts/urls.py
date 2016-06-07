61ab7dc083e6e95afc9a6516f2e94ce219be6ddd
seokholee <ssda0739@gmail.com>
2016-06-08 00:43:21 +0900

    logout 뷰를 등록하고, 템플릿 단에 signup, login,logout 링크추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import url
-from django.contrib.auth.views import login
+from django.contrib.auth.views import login, logout
 from . import views
 
 urlpatterns = [
-        url(r'^login/$', login, name='login'),
-        url(R'^signup/$', views.signup, name='signup'),
+        url(r'^signup/$', views.signup, name='signup'),
 ]
