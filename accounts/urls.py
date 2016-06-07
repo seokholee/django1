8bdb7ad53ab416f9dc5934f8c6b1c714c3c371aa
seokholee <ssda0739@gmail.com>
2016-06-08 00:47:05 +0900

    로그인 뷰 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,7 @@ from django.contrib.auth.views import login, logout
 from . import views
 
 urlpatterns = [
+        url(r'^login/$', login, name='login'),
+        url(r'^logout/$', login, name='logout'),
         url(r'^signup/$', views.signup, name='signup'),
 ]
